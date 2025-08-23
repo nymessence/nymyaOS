7d4dbe288e9d2381c822790f16c366ce5ee308da
humid_ray_0u@icloud.com <nymessence>
2025-08-22 23:37:40 -0400

    edited compile.sh to remove unnecessary mounts

--- nymya-core/compile.sh
+++ nymya-core/compile.sh
@@ -122,8 +122,6 @@ for TARGET in "${TARGETS[@]}"; do
     docker run --rm --platform=${DOCKER_PLATFORM[$TARGET]} \
         -w /nymyaOS/nymya-core \
         -v "$(pwd)":/nymyaOS/nymya-core \
-        -v "/lib/modules/${HOST_KERNEL_VERSION}/build":/lib/modules/${HOST_KERNEL_VERSION}/build \
-        -v /usr/src:/usr/src \
         -v "${KERNEL_SRC_COPY}":/nymyaOS/kernel-src:rw \
         "${IMAGE_NAME}" \
         bash -c "cd /nymyaOS/kernel-src && make mrproper && mkdir -p ${KERNEL_OUT_DIR} && make O=${KERNEL_OUT_DIR} ARCH=${ARCH_NAME} CROSS_COMPILE=${CROSS_COMPILE} defconfig && make O=${KERNEL_OUT_DIR} modules_prepare && make O=${KERNEL_OUT_DIR} M=/nymyaOS/nymya-core/kernel_syscalls/${DEB_ARCH} modules && make -C /nymyaOS/nymya-core deb-kernel"
