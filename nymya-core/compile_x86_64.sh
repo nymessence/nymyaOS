1ac71bd3a39f1f21457ef428e94f349517dee75f
humid_ray_0u@icloud.com <nymessence>
2025-08-24 04:28:29 -0400

    FIX: Disable RETPOLINE in kernel config

--- nymya-core/compile_x86_64.sh
+++ nymya-core/compile_x86_64.sh
@@ -35,13 +35,14 @@ apt-get update && apt-get install -y gcc-11
 export CC=gcc-11
 apt-get update && apt-get install -y gcc-11
 export CC=gcc-11
-make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" KCFLAGS="-mretpoline" defconfig
-make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" KCFLAGS="-mretpoline" modules_prepare
+make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" defconfig
+sed -i 's/CONFIG_RETPOLINE=y/# CONFIG_RETPOLINE is not set/' "${KERNEL_OUT_DIR}/.config"
+make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" modules_prepare
 
 # Build the kernel module
 echo "🔨 Building kernel module..."
 cd /nymyaOS/nymya-core
 make kernel PKG_ARCH=${DEB_ARCH}
-make -C "${KERNEL_OUT_DIR}" M="$(pwd)/kernel_syscalls/${DEB_ARCH}" ARCH="${ARCH}" KCFLAGS="-mretpoline" modules
+make -C "${KERNEL_OUT_DIR}" M="$(pwd)/kernel_syscalls/${DEB_ARCH}" ARCH="${ARCH}" modules
 
 echo "✅ Build completed for ${ARCH}."
