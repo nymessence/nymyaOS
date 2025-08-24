f8ffe7eb4d35944b39ac820ddfbdf6ab6434c06b
humid_ray_0u@icloud.com <nymessence>
2025-08-24 00:44:48 -0400

    FIX: Use KBUILD_CFLAGS with -Wno-error

--- nymya-core/compile_riscv64.sh
+++ nymya-core/compile_riscv64.sh
@@ -21,14 +21,13 @@ make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" defconfig
 sed -i 's/CONFIG_RETPOLINE=y/# CONFIG_RETPOLINE is not set/' "${KERNEL_OUT_DIR}/.config"
 make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" defconfig
 sed -i 's/CONFIG_RETPOLINE=y/# CONFIG_RETPOLINE is not set/' "${KERNEL_OUT_DIR}/.config"
-make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" defconfig
-sed -i 's/CONFIG_RETPOLINE=y/# CONFIG_RETPOLINE is not set/' "${KERNEL_OUT_DIR}/.config"
-make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" modules_prepare
+make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" KBUILD_CFLAGS="-mretpoline -Wno-error" defconfig
+make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" KBUILD_CFLAGS="-mretpoline -Wno-error" modules_prepare
 
 # Build the kernel module
 echo "🔨 Building kernel module..."
 cd /nymyaOS/nymya-core
 make kernel PKG_ARCH=${DEB_ARCH}
-make -C "${KERNEL_OUT_DIR}" M="$(pwd)/kernel_syscalls/${DEB_ARCH}" ARCH="${ARCH}" modules
+make -C "${KERNEL_OUT_DIR}" M="$(pwd)/kernel_syscalls/${DEB_ARCH}" ARCH="${ARCH}" KBUILD_CFLAGS="-mretpoline -Wno-error" modules
 
 echo "✅ Build completed for ${ARCH}."
