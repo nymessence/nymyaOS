4a0bceeb736a1c48d8a28c6e3bb867d6b8d30168
humid_ray_0u@icloud.com <nymessence>
2025-08-23 20:08:11 -0400

    REF: Update to Ubuntu 24.04 and simplify build logic

--- nymya-core/compile_riscv64.sh
+++ nymya-core/compile_riscv64.sh
@@ -17,13 +17,13 @@ echo "🚀 Starting build for ${ARCH}..."
 echo "🔧 Preparing kernel headers..."
 cd "${KERNEL_SRC_DIR}"
 make mrproper
-make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" HOSTCFLAGS="-mretpoline" defconfig
-make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" HOSTCFLAGS="-mretpoline" modules_prepare
+make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" defconfig
+make O="${KERNEL_OUT_DIR}" ARCH="${ARCH}" modules_prepare
 
 # Build the kernel module
 echo "🔨 Building kernel module..."
 cd /nymyaOS/nymya-core
 make kernel PKG_ARCH=${DEB_ARCH}
-make -C "${KERNEL_OUT_DIR}" M="$(pwd)/kernel_syscalls/${DEB_ARCH}" ARCH="${ARCH}" HOSTCFLAGS="-mretpoline" modules
+make -C "${KERNEL_OUT_DIR}" M="$(pwd)/kernel_syscalls/${DEB_ARCH}" ARCH="${ARCH}" modules
 
 echo "✅ Build completed for ${ARCH}."
